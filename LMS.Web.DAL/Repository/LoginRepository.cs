ddb4a26fd9a95704f45118e4e7ca178505fe310d
VedanshuJoshi <40860706+Vedanshu7@users.noreply.github.com>
2021-01-30 11:43:51 +0530

    Added Automaper And Login in BAL And DAL

---
+++ LMS.Web.DAL/Repository/LoginRepository.cs
@@ -0,0 +1,39 @@
+﻿using LMS.Web.DAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Web.DAL.Repository
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private static Database.LMSEntitiesAzure _db;
+
+        public LoginRepository()
+        {
+            _db = new  Database.LMSEntitiesAzure();
+        }
+        public int Login(string Email, string Password, int Role)
+        {
+            var Users =_db.Users.Find(Email);
+            if (Users != null)
+            {
+                if (Users.Password != Password)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else
+            {
+                return 3;
+            }
+
+        }
+    }
+}
