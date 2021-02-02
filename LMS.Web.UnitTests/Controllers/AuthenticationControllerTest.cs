0d7baaaec410137d495ce2b383d7ee4f601737cf
Mihir Joshi <mihir67mj@gmail.com>
2021-02-02 17:31:19 +0530

    Added Sample Unit Tests (#12)
    * User Detail screen added.
    
    In this I have added user list screen which display list of user available in database.
    
    * Added UnitTest Project, Created Sample Unit Test

---
+++ LMS.Web.UnitTests/Controllers/AuthenticationControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LMS.Web.Controllers;
+using System.Web.Mvc;
+using LMS.Web.BAL.Manager;
+using LMS.Web.BAL.ViewModels;
+using LMS.Web.UnitTests.Repository;
+
+namespace LMS.Web.UnitTests
+{
+    [TestClass]
+    public class AuthenticationControllerTest
+    {
+        [TestMethod]
+        public void Login_ShouldReturnSuccess()
+        {
+            var controller = new AuthenticationController(new LoginManager(new TestLoginRepository()));
+
+            LoginViewModel testUser = new LoginViewModel() { Email = "test@test.com", Password = "Lms@2021", Role = 1 };
+
+            var result = controller.Login(testUser);
+
+            Assert.AreEqual("Success", result);
+        }
+    }
+}
