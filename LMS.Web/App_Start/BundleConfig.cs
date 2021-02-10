f018c14e6154b08099f2ca7d1970f29e888eac89
Vedanshu <vedanshujoshi50@gmail.com>
2021-02-10 10:13:43 +0530

    Add Datatable layout

--- LMS.Web/App_Start/BundleConfig.cs
+++ LMS.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace LMS.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/adminlte/plugins/jquery/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,16 +23,37 @@ namespace LMS.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                         "~/Scripts/bootstrap.bundle.min.js"));
 
-            bundles.Add(new ScriptBundle("~/adminlte/js").Include(
-                        "~/adminlte/js/adminlte.min.js",
-                        "~/adminlte/plugins/summernote/summernote-bs4.min.js"
-                        ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+
+                     bundles.Add(new ScriptBundle("~/adminlte/js").Include(
+                        "~/adminlte/js/adminlte.min.js",
+                        "~/adminlte/js/demo.js",
+                        "~/adminlte/plugins/summernote/summernote-bs4.min.js",
+                        "~/adminlte/plugins/jquery/jquery.min.js",
+                        "~/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/adminlte/plugins/datatables/jquery.dataTables.min.js",
+                        "~/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js",
+                        "~/adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js",
+                        "~/adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js",
+                        "~/adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js",
+                        "~/adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js",
+                        "~/adminlte/plugins/jszip/jszip.min.js",
+                        "~/adminlte/plugins/pdfmake/pdfmake.min.js",
+                        "~/adminlte/plugins/pdfmake/vfs_fonts.js",
+                        "~/adminlte/plugins/datatables-buttons/js/buttons.html5.min.js",
+                        "~/adminlte/plugins/datatables-buttons/js/buttons.print.min.js",
+                        "~/adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"
+                        ));
+        
+
+                          bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
                         "~/adminlte/css/adminlte.min.css",
                         "~/adminlte/plugins/fontawesome-free/css/all.min.css",
-                        "~/adminlte/plugins/summernote/summernote-bs4.min.css"
+                        "~/adminlte/plugins/summernote/summernote-bs4.min.css",
+                        "~/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css",
+                        "~/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css",
+                        "~/adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css"
                         ));
             bundles.Add(new StyleBundle("~/Content/LoginCss").Include(
                  "~/Content/site.css"
